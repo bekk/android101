c188687cc0ab0fd8303b50d293a9eee3a9c5ba5e
Christoffer Marcussen <christoffer.marcussen@bekk.no>
2014-08-16 12:20:09 +0200

    Byttet fra ListActivity til Activity

--- SpotifyApiEksempel/apieksempel/src/main/java/no/bekk/spotifyapieksempel/ResultActivity.java
+++ SpotifyApiEksempel/apieksempel/src/main/java/no/bekk/spotifyapieksempel/ResultActivity.java
@@ -1,10 +1,12 @@
 package no.bekk.spotifyapieksempel;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,12 +20,15 @@ import no.bekk.spotifyapieksempel.adapters.TrackAdapter;
 import no.bekk.spotifyapieksempel.domain.Track;
 
 
-public class ResultActivity extends ListActivity {
+public class ResultActivity extends Activity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        listView = (ListView) findViewById(R.id.listView);
 
         try {
             // Get the track list from the MainActivity
@@ -31,31 +36,32 @@ public class ResultActivity extends ListActivity {
             ObjectMapper mapper = new ObjectMapper();
             List<Track> tracks = Arrays.asList(mapper.readValue(data.getString("tracks"), Track[].class));
 
-            setListAdapter(new TrackAdapter(this, R.layout.result_list_item, tracks));
+            listView.setAdapter(new TrackAdapter(this, R.layout.result_list_item, tracks));
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    // Get the clicked track
+                    Track track = (Track) parent.getItemAtPosition(position);
+                    // Start playback in Spotify
+                    Intent appPlayBackIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(track.getHref()));
+                    // Verify that the device has Spotify and can play the track
+                    if (appPlayBackIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(appPlayBackIntent);
+                    }
+                    else {
+                        // Open play store so that the user can download Spotify
+                        String packageName = "com.spotify.mobile.android.ui";
+                        try {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
+                        }
+                        catch (android.content.ActivityNotFoundException anfe) {
+                            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + packageName)));
+                        }
+                    }
+                }
+            });
         } catch (IOException e) {
             // Do nothing
-        }
-    }
-
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
-        // Get the clicked track
-        Track track = (Track) getListAdapter().getItem(position);
-        // Start playback in Spotify
-        Intent appPlayBackIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(track.getHref()));
-        // Verify that the device has Spotify and can play the track
-        if (appPlayBackIntent.resolveActivity(getPackageManager()) != null) {
-            startActivity(appPlayBackIntent);
-        }
-        else {
-            // Open play store so that the user can download Spotify
-            String packageName = "com.spotify.mobile.android.ui";
-            try {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
-            }
-            catch (android.content.ActivityNotFoundException anfe) {
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + packageName)));
-            }
         }
     }
 }
