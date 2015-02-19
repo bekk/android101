fba5bfcfc3051b915c70a66b27a406c11a96bf37
Christoffer Marcussen <christoffer.marcussen@bekk.no>
2015-02-19 20:23:51 +0100

    Button listener for fab

--- SpotifyLollipop/app/src/main/java/no/bekk/lollipop/MainActivity.java
+++ SpotifyLollipop/app/src/main/java/no/bekk/lollipop/MainActivity.java
@@ -1,5 +1,7 @@
 package no.bekk.lollipop;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
@@ -10,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,10 +30,13 @@ public class MainActivity extends ActionBarActivity {
     private RecyclerView.LayoutManager mLayoutManager;
     private DrawerLayout Drawer;
     private ActionBarDrawerToggle mDrawerToggle;
+    private FloatingActionButton floatingActionButton;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = this;
         setContentView(R.layout.activity_main);
         listItems.add("Search");
         icons.add(R.mipmap.ic_action_music_1);
@@ -37,6 +44,7 @@ public class MainActivity extends ActionBarActivity {
         setupToolbar();
         setupRecyclerView();
         setupDrawer();
+        setupFAB();
     }
 
     @Override
@@ -85,5 +93,16 @@ public class MainActivity extends ActionBarActivity {
         Drawer.setDrawerListener(mDrawerToggle);
         mDrawerToggle.setDrawerIndicatorEnabled(true);
         mDrawerToggle.syncState();
+    }
+
+    private void setupFAB() {
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab_search);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, SearchActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
     }
 }
